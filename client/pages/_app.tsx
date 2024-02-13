15c06788a51ff57588aa064d820716a3357802cb
take5150 <watanabetakehiro5150@gmail.com>
2024-02-14 07:42:08 +0900

    コンポーネント作成

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,13 @@
+import Navbar from "@/src/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      {/* アプリ全体で表示するために_app.tsxに記載 */}
+      <Navbar></Navbar>
+      <Component {...pageProps} />;
+    </div>
+  )
 }

0329106c7bb5c3cb2b5494d5d81d553eb5767c2a
take5150 <watanabetakehiro5150@gmail.com>
2024-02-13 21:49:31 +0900

    ユーザAPI追加

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const authRoute = require("./routers/auth.js");
 
 const PORT = 5000;
+// JSON形式のデータを扱うことを宣言
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`server is running PORT ${PORT}`));

00303647bbe942c99291b05469aa4195efa60eda
take5150 <watanabetakehiro5150@gmail.com>
2024-02-14 20:14:41 +0900

    user create api from front

--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,7 +1,34 @@
+import apiClient from '@/src/lib/apiClient'
 import Head from 'next/head'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
 
 const signup = () => {
+
+  const [username, setUserName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // 新規登録を行うAPIをたたく
+    try {
+      await apiClient.post("/auth/register", {
+        username,
+        email,
+        password
+      });
+
+      router.push("/login")
+    }catch(err) {
+      alert("入力が不正です。")
+      console.log(err)
+    }
+    
+  };
+
   return (
     <div
       style={{ height: "88vh" }}
@@ -17,7 +44,7 @@ const signup = () => {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="email"
@@ -32,6 +59,7 @@ const signup = () => {
                 autoComplete="name"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserName(e.target.value)}
               />
             </div>
             <div className="mt-6">
@@ -48,6 +76,7 @@ const signup = () => {
                 autoComplete="email"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
               />
             </div>
             <div className="mt-6">
@@ -64,6 +93,7 @@ const signup = () => {
                 autoComplete="new-password"
                 required
                 className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
               />
             </div>
             <div className="mt-6">
